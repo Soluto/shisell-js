719ebd78fe8d671999fb94b2f56c22310e014c78
Aviv Rozenboim <aviv@soluto.com>
2017-07-17 12:58:52 +0300

    Added TS typedef file

---
+++ index.d.ts
@@ -0,0 +1,44 @@
+declare module 'shisell' {
+    export type AnalyticsEventModel = {
+        Name: string;
+        Scope: string;
+        ExtraData: {[key: string]: any};
+        MetaData: {[key: string]: any};
+        Identities: {[key: string]: any};
+    };
+
+    export type DispatchAnalytics = (eventName: string, context?: AnalyticsContext) => Promise<AnalyticsEventModel>;
+
+    export type EventModelWriter<T> = (event: AnalyticsEventModel) => Promise<T>;
+
+    export type AnalyticsFilter = (
+        dispatcher: AnalyticsDispatcher
+    ) => Promise<AnalyticsDispatcher> | AnalyticsDispatcher;
+
+
+    export class AnalyticsContext {
+        Scopes: string[];
+        ExtraData: {[key: string]: any};
+        MetaData: {[key: string]: any};
+        Filters: AnalyticsFilter[];
+        Identities: {[key: string]: any};
+    }
+
+    export class AnalyticsDispatcher {
+        constructor(dispatch: DispatchAnalytics, context: AnalyticsContext);
+        withContext(context: AnalyticsContext): AnalyticsDispatcher;
+        createScoped(scope: string): AnalyticsDispatcher;
+        withExtra(key: string, value: any): AnalyticsDispatcher;
+        withFilter(filter: AnalyticsFilter): AnalyticsDispatcher;
+        withMeta(key: string, value: any): AnalyticsDispatcher;
+        withIdentity(key: string, value: any): AnalyticsDispatcher;
+        dispatch: DispatchAnalytics;
+    }
+
+    export function createRootDispatcher<T>(writer: EventModelWriter<T>, rootContext?: AnalyticsContext): AnalyticsDispatcher;
+
+    export var writers: {
+        console: EventModelWriter<void>;
+        mixpanel: (id: string) => EventModelWriter<void>;
+    };
+}
