d64ee8a3d2c2801c4ff789fed5fbea3ec9841b6a
Nataly Shrits <nataly87s@gmail.com>
2018-10-15 13:47:21 +0300

    fix Error: Cannot call a namespace ('deepmerge') (#37)
    * fix Error: Cannot call a namespace ('deepmerge')
    
    * bump version
    
    v1.0.1

--- rollup.config.js
+++ rollup.config.js
@@ -1,10 +1,22 @@
+import typescript from 'rollup-plugin-typescript2';
 import commonjs from 'rollup-plugin-commonjs';
 import resolve from 'rollup-plugin-node-resolve';
 import {uglify} from 'rollup-plugin-uglify';
 
 export default {
-  input: './dist/_rollup/internal/umd.js',
-  plugins: [resolve(), commonjs(), uglify()],
+  input: './src/internal/umd.ts',
+  plugins: [
+    resolve(),
+    commonjs(),
+    typescript({
+      tsconfigOverride: {
+        compilerOptions: {
+          module: 'esnext',
+        },
+      },
+    }),
+    uglify(),
+  ],
   output: [
     {
       file: 'dist/bundles/shisell.umd.js',
